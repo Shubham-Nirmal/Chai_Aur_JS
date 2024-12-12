4c0386087dc6738be11e2d1a081dd97e70b75acf
Shubham-Nirmal <shubhamnirmal2612@gmail.com>
2024-12-12 13:22:08 +0000

    datatype

---
+++ 02_data_types.js
@@ -0,0 +1,10 @@
+"use strict ";//treat all js code as newer version //
+
+//alert(3+ 3) ; we are using nodejs, not browser//
+
+
+console.log(3 
+    + 
+    3) //code readability should be high //
+
+    
