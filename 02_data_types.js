7c082cc13051d5c311499884e038ff94d815d84a
Shubham-Nirmal <shubhamnirmal2612@gmail.com>
2024-12-12 14:14:21 +0000

    datatypes

--- 02_data_types.js
+++ 02_data_types.js
@@ -7,4 +7,28 @@ console.log(3
     + 
     3) //code readability should be high //
 
-    
+    console.log ("Hitesh")
+
+    let name= "Hitesh"
+
+    let age = 21
+     
+    let isloggendIn = false 
+
+
+    //Data Type 
+
+    //nuber => 2 to power 53 
+    //bigint  jast use nahi karanar but no motha asel teva karav lagAL EX, Treding chya veles.
+    //string => " "
+    //boolean => true/ false 
+    //null => standalone value ()
+    //undefind=> ( value defind zaleli nahiye )
+      //symbol=> unique 
+
+      //object
+
+      console.log(typeof age)//out put null
+      
+      console.log(typeof null)// out put object karan null he object he 
+      console.log(typeof undefined) //out put undefined ani undefined ak type he jas ki string sarkh no sarkh (imp point iw )
